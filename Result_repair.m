b6ed706d260c01495ca7d6888ea03bf0d8273480
Ikent1101 <88181071+Ikent1101@users.noreply.github.com>
2022-12-28 23:41:07 +0900

    Update Result_repair.m

--- Result_repair.m
+++ Result_repair.m
@@ -1,7 +1,15 @@
 clear;
 con=0;
 count=7;
+cou=1;
 fl=('E:\Document\MATLAB\'+string(count)+'/ResultData.txt');
 opts2=detectImportOptions(fl,'Encoding','UTF-8','ReadVariableNames',false,'ExtraColumnsRule','wrap');% サンプルデータのデータ形式判定
 hdat2=readmatrix(fl, opts2,'Encoding','UTF-8'); % サンプルデータを表として読み込む
 ResultData=array2table(hdat2,'VariableNames',{'ID','SetNum','TarPos','SabPos1','SabPos2','Fro','Mid','Bott','c/f','Reaction'});
+while 1
+    
+    cou=cou+1;
+    if isequal(height(ResultData)+1,cou)
+        break;
+    end
+end 
