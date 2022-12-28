9b163b0c2226157d02e66752faf60c9d0f6dac09
Ikent1101 <88181071+Ikent1101@users.noreply.github.com>
2022-12-29 04:17:27 +0900

    Update repair.m

--- repair.m
+++ repair.m
@@ -1,5 +1,5 @@
 clear;
-count=8;
+count=9;
 %8+9のようにデータが連なってるので、8ではデータ下を消す
 %同様に9ではデータ上を消す
 for i=0:108
@@ -8,8 +8,10 @@ for i=0:108
     EyeSpot=readmatrix(fl, opts);% サンプルデータを表として読み込む
     con=2;
     while 1
-        if EyeSpot(con-1,5)>EyeSpot(con,5)
-            EyeSpot(con:end,:)=[];
+        if EyeSpot(con,5)>EyeSpot(con+1,5)
+            %EyeSpot(con:end,:)=[];
+            %EyeSpot(con,1)=5;
+            EyeSpot(1:con,:)=[];
             writematrix(EyeSpot,'E:\Document\MATLAB\'+string(count)+'/EyeTracking2/'+string(i)+'.txt');
            break;
         elseif height(EyeSpot)==con+1
