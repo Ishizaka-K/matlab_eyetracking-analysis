3291c01b71dd037632da30712ccde48002434a73
Ikent1101 <88181071+Ikent1101@users.noreply.github.com>
2022-12-29 04:10:14 +0900

    Update repair.m

--- repair.m
+++ repair.m
@@ -3,13 +3,14 @@ count=8;
 %8+9のようにデータが連なってるので、8ではデータ下を消す
 %同様に9ではデータ上を消す
 for i=0:108
-    fl=('E:\Document\MATLAB\'+string(count)+'/EyeTracking/'+string(i)+'.txt');
+    fl=('E:\Document\MATLAB\'+string(count)+'/EyeTracking2/'+string(i)+'.txt');
     opts=detectImportOptions(fl); % サンプルデータのデータ形式判定
     EyeSpot=readmatrix(fl, opts);% サンプルデータを表として読み込む
     con=2;
     while 1
         if EyeSpot(con-1,5)>EyeSpot(con,5)
             EyeSpot(con:end,:)=[];
+            writematrix(EyeSpot,'E:\Document\MATLAB\'+string(count)+'/EyeTracking2/'+string(i)+'.txt');
            break;
         elseif height(EyeSpot)==con+1
            break;
