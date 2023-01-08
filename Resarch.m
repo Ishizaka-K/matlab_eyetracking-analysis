f4aeb13dec24ab6309cd2f31ba48ea7f655c3820
Ikent1101 <88181071+Ikent1101@users.noreply.github.com>
2023-01-08 12:28:24 +0900

    Update Resarch.m

--- Resarch.m
+++ Resarch.m
@@ -1,3 +1,6 @@
+clear;
 for i=1:14
-    fl=[]
+    fl=('E:\Document\MATLAB\'+string(i)+'/eyeData.txt');
+    opts2=detectImportOptions(fl,'Encoding','UTF-8','ReadVariableNames',false,'ExtraColumnsRule','wrap');% サンプルデータのデータ形式判定
+    hdat2=readmatrix(fl, opts2,'Encoding','UTF-8');
 end
