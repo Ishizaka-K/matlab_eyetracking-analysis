9ecfd243e68394e66d13f13dc2b35aa15836f2b7
Ikent1101 <88181071+Ikent1101@users.noreply.github.com>
2023-01-08 12:28:27 +0900

    Create Repairtime.m

---
+++ Repairtime.m
@@ -0,0 +1,37 @@
+clear;
+for count=1:14
+
+fl=('E:\Document\MATLAB\'+string(count)+'/eyeData.txt');
+opts2=detectImportOptions(fl,'Encoding','UTF-8','ReadVariableNames',false,'ExtraColumnsRule','wrap');% サンプルデータのデータ形式判定
+hdat2=readmatrix(fl, opts2,'Encoding','UTF-8'); % サンプルデータを表として読み込む
+
+
+%zeroの読み込みa
+for i=1:108
+    fl=('E:\Document\MATLAB\'+string(count)+'/EyeTracking/'+string(i)+'.txt');
+    opts=detectImportOptions(fl); % サンプルデータのデータ形式判定
+    EyeSpot=readmatrix(fl, opts);% サンプルデータを表として読み込む
+    if hdat2(i,4)~=0
+        response=EyeSpot(hdat2(i,4),5)-EyeSpot(1,5);
+        hdat2(i,4)=response;
+    else 
+    end
+end
+for i=1:108
+    fl=('E:\Document\MATLAB\'+string(count)+'/EyeTracking2/'+string(i)+'.txt');
+    opts=detectImportOptions(fl); % サンプルデータのデータ形式判定
+    EyeSpot=readmatrix(fl, opts);% サンプルデータを表として読み込む
+    if hdat2(108+i,4)~=0
+        if height(EyeSpot)<hdat2(108+i,4)
+            response=0;
+        else
+            response=EyeSpot(hdat2(108+i,4),5)-EyeSpot(1,5);
+        end
+        hdat2(108+i,4)=response;
+    else 
+    end
+end
+
+eData=array2table(hdat2,'VariableNames',{'TragetPosition','Xposition','Yposition','timig'});
+writetable(eData,'E:\Document\MATLAB\'+string(count)+'/eyeData.txt');
+end
