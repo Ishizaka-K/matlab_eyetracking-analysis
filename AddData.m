2aa095da0c37d0d7e9905ca48f25912b356ea1e1
Ikent1101 <88181071+Ikent1101@users.noreply.github.com>
2023-01-12 22:22:52 +0900

    Update AddData.m

--- AddData.m
+++ AddData.m
@@ -6,20 +6,19 @@ hdat1=readmatrix('E:\Document\Asoturon\ResultData\'+string(count)+'Resultdata.cs
 hdat2=readmatrix('E:\Document\Asoturon\ResultData\'+string(count)+'Resultdata2.csv');
 hdat3=[hdat1;hdat2];
 %eyedata(eyedata(:,1)==0&eyedata(:,2)==0&eyedata(:,3)==0&eyedata(:,4)==0,:)=[];
+no=[0,0,0,0,0,0,0,0,0,0];
+hdat3=[no;hdat3];
 F=0;%6
 M=0;%7
 B=0;%8
-i=1;
+i=2;
 while 1
-if F<sum(hdat3(1:i,6))
+if hdat3(i-1,6)~=hdat3(i,6)
     depth(i,1)=1;
-    F=sum(hdat3(1:i,6));
-elseif M<sum(hdat3(1:i,7))
+elseif hdat3(i-1,7)~=hdat3(i,7)
     depth(i,1)=2;
-    M=sum(hdat3(1:i,7));
-elseif B<sum(hdat3(1:i,8))
+elseif hdat3(i-1,8)~=hdat3(i,8)
     depth(i,1)=3;
-    B=sum(hdat3(1:i,8));
 else
     disp("erro");
 end
@@ -30,7 +29,9 @@ end
 
 i=i+1;
 end
-
+depth(depth==0)=[];
 add_data=[eyedata,depth];
+eData=array2table(add_data,'VariableNames',{'Traget','Xposition','Yposition','timig','depth'});
+writetable(eData,'E:\Document\Asoturon\EyeSacker\'+string(count)+'EyeSacker.csv');
 clear;
 end
