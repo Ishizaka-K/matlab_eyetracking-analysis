1edc5f9b79bb836620bd3295f8c36a3b80a0dc8a
Ikent1101 <88181071+Ikent1101@users.noreply.github.com>
2023-01-17 20:26:53 +0900

    Update meaneye.m

--- meaneye.m
+++ meaneye.m
@@ -1,7 +1,11 @@
 clear;
-svdata=[0,0,0,0,0];
+svdata=[0,0,0,0,0,0,0];
 for count=1:14
-hdat2=xlsread('E:\Document\Asoturon\EyeSacker\'+string(count)+'EyeSacker.csv'); % サンプルデータを表として読み込む
+hda1=readmatrix('E:\Document\Asoturon\ResultData\'+string(count)+'Resultdata.csv');
+hda2=readmatrix('E:\Document\Asoturon\ResultData\'+string(count)+'Resultdata2.csv');
+hda3=[hda1(:,2);hda2(:,2)];
+hdat2=xlsread('E:\Document\Asoturon\EyeSacker\'+string(count)+'EyeSacker.csv'); 
+hdat2=[hdat2,hda3];
 zerdata=find(hdat2(:,4)==0);
 hdat2(zerdata,:)=[];
 zerdata=find(hdat2(:,2)==0);
@@ -26,12 +30,12 @@ scatter(svdata(1:(end-1),2),svdata(1:(end-1),3))
 xlim([-0.5 0.5])
 ylim([-0.5 0.5]);
 
-eData=array2table(svdata,'VariableNames',{'TragetPosition','Xposition','Yposition','time','depth'});
-writetable(eData,'E:\Document\MATLAB\AllEyeData.csv');
+% eData=array2table(svdata,'VariableNames',{'TragetPosition','Xposition','Yposition','time','depth'});
+% writetable(eData,'E:\Document\MATLAB\AllEyeData.csv');
 
-data0=[0,0,0,0,0];
-data1=[0,0,0,0,0];
-data2=[0,0,0,0,0];
+data0=[0,0,0,0,0,0,0];
+data1=[0,0,0,0,0,0,0];
+data2=[0,0,0,0,0,0,0];
 for j=1:height(svdata)
     if svdata(j,5)==1
     data0=[data0;svdata(j,:)];
@@ -44,11 +48,33 @@ end
 data0(1,:)=[];
 data1(1,:)=[];
 data2(1,:)=[];
+Spotdata1=data0(data0(:,7)==0|data0(:,7)==1,:);
+Spotdata2=data0(data0(:,7)==2|data0(:,7)==3,:);
+Spotdata3=data0(data0(:,7)==4|data0(:,7)==5,:);
+figure
+scatter(Spotdata1(:,2),Spotdata1(:,3))
+xlim([-0.5 0.5])
+ylim([-0.5 0.5]);
+
+figure
+scatter(Spotdata1(:,2),Spotdata1(:,3))
+xlim([-0.5 0.5])
+ylim([-0.5 0.5]);
+
+
+figure
+scatter(Spotdata3(:,2),Spotdata3(:,3))
+xlim([-0.5 0.5])
+ylim([-0.5 0.5]);
+
+
+
 alavdata(2,:)=[1,mean(data1(:,2)),mean(data0(:,3))];
 alavdata(3,:)=[2,mean(data2(:,2)),mean(data0(:,3))];
 
-alData=array2table(alavdata,'VariableNames',{'ID','X','Y'});
-writetable(alData,'E:\Document\MATLAB\AllaverageData.csv');
+
+% alData=array2table(alavdata,'VariableNames',{'ID','X','Y'});
+% writetable(alData,'E:\Document\MATLAB\AllaverageData.csv');
 
 
 figure
