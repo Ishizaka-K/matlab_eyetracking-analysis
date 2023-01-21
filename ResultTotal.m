c658e9f4f9e4954039f34f43f36540d1dc985c73
Ikent1101 <88181071+Ikent1101@users.noreply.github.com>
2023-01-21 23:50:27 +0900

    Update ResultTotal.m

--- ResultTotal.m
+++ ResultTotal.m
@@ -1,7 +1,4 @@
 clear;
-data0=zeros(1,10);
-data1=zeros(1,10);
-data2=zeros(1,10);
 
 for count=1:14
 disp(count);
@@ -11,46 +8,61 @@ hdat3=[hdat1;hdat2];
 data0=zeros(1,10);
 data1=zeros(1,10);
 data2=zeros(1,10);
-hdat3=[0,0,0,0,0,0,0,0,0,0;hdat3];
 disp(hdat3(hdat3(:,10)>5,:));
 hdat3(hdat3(:,10)>5,:)=[];
+<<<<<<< Updated upstream
+=======
+% hdat3((hdat3(:,9)==0),:)=[];
+hdat3((hdat3(:,1)==0),:)=[];
+
+
+>>>>>>> Stashed changes
 for j=1:height(hdat3)
-    if hdat3(j,5)==1
+    if hdat3(j,2)==0|hdat3(j,2)==1|hdat3(j,2)==2|hdat3(j,2)==3|hdat3(j,2)==4|hdat3(j,2)==5
     data0=[data0;hdat3(j,:)];
-    elseif hdat3(j,5)==2
+    elseif hdat3(j,2)==6|hdat3(j,2)==7|hdat3(j,2)==8|hdat3(j,2)==9|hdat3(j,2)==10|hdat3(j,2)==11
     data1=[data1;hdat3(j,:)];
-    elseif hdat3(j,5)==3
+    elseif hdat3(j,2)==12|hdat3(j,2)==17|hdat3(j,2)==13|hdat3(j,2)==14|hdat3(j,2)==15|hdat3(j,2)==16
     data2=[data2;hdat3(j,:)];
     end
 end
 
+data0(data0(:,1)==0,:)=[];
+data1(data1(:,1)==0,:)=[];
+data2(data2(:,1)==0,:)=[];
+
+
 %F_icchi
-icchi(count,1)=mean(data0(data0(:,2)==0|data0(:,2)==1,10));
+icchi(count,1)=mean(data0(data0(:,2)==0|data0(:,2)==1,9));
 %F_hui
-hui(count,1)=mean(data0(data0(:,2)==2|data0(:,2)==3,10));
+hui(count,1)=mean(data0(data0(:,2)==2|data0(:,2)==3,9));
 %F_no
-no(count,1)=mean(data0(data0(:,2)==4|data0(:,2)==5,10));
+no(count,1)=mean(data0(data0(:,2)==4|data0(:,2)==5,9));
 
 %M_icchi
-icchi(count,2)=mean(data1(data1(:,2)==7|data1(:,2)==8,10));
+icchi(count,2)=mean(data1(data1(:,2)==7|data1(:,2)==8,9));
 %M_hui
-hui(count,2)=mean(data1(data1(:,2)==6|data1(:,2)==9,10));
+hui(count,2)=mean(data1(data1(:,2)==6|data1(:,2)==9,9));
 %M_no
-no(count,2)=mean(data1(data1(:,2)==10|data1(:,2)==11,10));
+no(count,2)=mean(data1(data1(:,2)==10|data1(:,2)==11,9));
 
 %B_icchi
-icchi(count,3)=mean(data2(data2(:,2)==14|data2(:,2)==15,10));
+icchi(count,3)=mean(data2(data2(:,2)==14|data2(:,2)==15,9));
 %B_hui
-hui(count,3)=mean(data2(data2(:,2)==12|data2(:,2)==13,10));
+hui(count,3)=mean(data2(data2(:,2)==12|data2(:,2)==13,9));
 %B_no
-no(count,3)=mean(data2(data2(:,2)==16|data2(:,2)==17,10));
+no(count,3)=mean(data2(data2(:,2)==16|data2(:,2)==17,9));
 
 clear hdat3 data0 data1 data2;
 end
 total=[icchi,hui,no];
 TotalR=array2table(total,'VariableNames',{'F_icchi','M_icchi','B_icchi','F_hui','M_hui','B_hui', ...
     'F_no','M_no','B_no'});
+<<<<<<< Updated upstream
 writetable(TotalR,'E:\Document\Asoturon\TotalVariable.csv');
+=======
+writetable(TotalR,'E:\Document\Asoturon\TotalCorrect2.csv');
+>>>>>>> Stashed changes
 
 
 
